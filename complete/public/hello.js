c8f60a77dc130a67052ee893aed255eada95b48a
joseverdugo <jose.verdugo@tinet.cl>
2016-09-12 17:32:50 -0300

    Test utlizando *

--- complete/public/hello.js
+++ complete/public/hello.js
@@ -6,4 +6,13 @@ $(document).ready(function() {
        $('.greeting-content').append(data.content);
        console.log(jqxhr);
     });
+    
+    
+    $.ajax({
+        url: "http://localhost:8080/greeting-javaconfig"
+    }).then(function(data, status, jqxhr) {
+       $('.greeting-id-2').append(data.id);
+       $('.greeting-content-2').append(data.content);
+       console.log(jqxhr);
+    });
 });
